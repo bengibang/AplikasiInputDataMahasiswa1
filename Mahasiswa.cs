7e95e858d6d1586a9342bc92b2b7f359da157ad3
bengibang <bengibang955@gmail.com>
2021-10-23 15:22:29 +0700

    projet kedua

---
+++ Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public string Nim { get; internal set; }
+        public string Nama { get; internal set; }
+        public string Kelas { get; internal set; }
+        public int Nilai { get; internal set; }
+        public string NilaiHuruf { get; internal set; }
+    }
+}
